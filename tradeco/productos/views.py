f6b73974d0d948d67026d54fa3f210a2b3fcdaaa
rustico <nmbases@gmail.com>
2013-08-11 04:16:05 -0300

    Breadcrumb en producto detalle

--- tradeco/productos/views.py
+++ tradeco/productos/views.py
@@ -26,4 +26,15 @@ def index(request, categoria = None, sub_categoria = None):
 def detalle(request, producto):
     producto = get_object_or_404(Producto, slug = producto)
     relacionados = producto.categoria.producto_set.all()[:5]
-    return TemplateResponse(request, 'productos/detalle.html', { 'producto' : producto, 'relacionados':relacionados })
+
+    breadcrumb = [('Home', '/')]
+    categoria_url = reverse('productos.views.index', args=(producto.categoria.slug,))
+    breadcrumb.append((producto.categoria.nombre, categoria_url))
+    if producto.sub_categoria:
+        sub_categoria_url = reverse('productos.views.index', args=(producto.categoria.slug, producto.sub_categoria.slug))
+        breadcrumb.append((producto.sub_categoria.nombre, sub_categoria_url))
+
+    producto_url = reverse('productos.views.detalle', args=(producto.slug,))
+    breadcrumb.append((producto.titulo, producto_url))
+
+    return TemplateResponse(request, 'productos/detalle.html', { 'producto' : producto, 'relacionados':relacionados,'breadcrumb': breadcrumb } )
