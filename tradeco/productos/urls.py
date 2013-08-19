5d5dc6d10ddb78fb24d07692e7cf2529accd79f0
rustico <nmbases@gmail.com>
2013-08-19 13:31:50 -0300

    Se termino el paginador

--- tradeco/productos/urls.py
+++ tradeco/productos/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import url, patterns, include
 
 urlpatterns = patterns('productos.views',
     url(r'^producto/(?P<producto>[a-z0-9\-]+)?$', 'detalle'),
-    url(r'^(?P<categoria>\w+)?/(?P<sub_categoria>\w+)?$', 'index'),
-    url(r'^(?P<categoria>\w+)?$', 'index'),
+    url(r'^(?P<categoria>[a-z0-9\-]+)?/(?P<sub_categoria>[a-z0-9\-]+)?$', 'index'),
+    url(r'^(?P<categoria>[a-z0-9\-]+)?$', 'index'),
 )
 
 #urlpatterns += patterns('', 
